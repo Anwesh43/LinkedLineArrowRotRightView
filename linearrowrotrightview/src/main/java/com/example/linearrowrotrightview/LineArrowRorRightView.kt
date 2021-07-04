16662f8329464d784efbabb04c0191628ba7ecbc
Anwesh43 <anweshthecool0@gmail.com>
2021-07-05 01:30:57 +0530

    adding LineArrowRorRightView class

--- linearrowrotrightview/src/main/java/com/example/linearrowrotrightview/LineArrowRorRightView.kt
+++ linearrowrotrightview/src/main/java/com/example/linearrowrotrightview/LineArrowRorRightView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawLARRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineArrowRotRight(scale, w, h, paint)
 }
+
+class LineArrowRorRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
